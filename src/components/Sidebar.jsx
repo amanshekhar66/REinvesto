cfeefcd4ab8e0418a299d7c952f27d113978ad36
amanshekhar66 <amanshekhar66@gmail.com>
2023-05-23 20:12:32 +0530

    second

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const App = () => {
+  return (
+    <div>Sidebar</div>
+    )
+}
+
+export default Sidebar
---
+++ src/pages/CampaignDetails.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const App = () => {
+  return (
+    <div>CampaignDetails</div>
+    )
+}
+
+export default CampaignDetails
